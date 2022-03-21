15a93af896d53c5d980a0b8883dbebd23bc6ced7
audreyjy <audrey.youngberg1@gmail.com>
2022-03-21 09:05:38 -0600

    Start project, create models and database, add views

---
+++ LaytonTemple/Models/AvailableTimes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaytonTemple.Models
+{
+    public class AvailableTimes
+    {
+        [Required]
+        [Key]
+        public int TimeId { get; set; }
+
+        public string Time { get; set; }
+
+    }
+}
